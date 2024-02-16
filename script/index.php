ff91b9f93b7d5d3e613d6d3c8df46d2ba80a1533
AlessiaOsman <alessia.osman@gmail.com>
2024-02-16 18:09:48 +0100

    completed

--- script/index.php
+++ script/index.php
@@ -1,2 +1,23 @@
 <?php 
-echo ('Ciao');
+$paragraph = $_GET['paragraph'];
+$badword = $_GET['bedwords'];
+
+$paragraph_len = strlen($paragraph);
+
+$new_paragraph = str_replace($badword, '***', $paragraph);
+$new_paragraph_len = strlen($new_paragraph);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?= "$paragraph $paragraph_len" ?></h1>
+    <h1><?= "$new_paragraph $new_paragraph_len" ?></h1>
+</body>
+</html>
